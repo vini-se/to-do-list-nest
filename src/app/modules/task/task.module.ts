4df1be0210a77de95423c75ed4057ff4d3539a36
Vinicius <vinicius.dla2106@gmail.com>
2025-04-13 16:17:55 -0300

    feat: create task module

---
+++ src/app/modules/task/task.module.ts
@@ -0,0 +1,55 @@
+import { TaskEntity } from '@/domain/entities/task.entity';
+import { UserEntity } from '@/domain/entities/user.entity';
+import { TaskRepository } from '@/domain/repositories/task.repository';
+import { CreateTaskUseCase } from '@/domain/use-cases/task/create.task.use-case';
+import { DeleteTaskUseCase } from '@/domain/use-cases/task/delete.task.use-case';
+import { ListByFilterTaskUseCase } from '@/domain/use-cases/task/list-by-filter.task.use-case';
+import { ListOneTaskUseCase } from '@/domain/use-cases/task/list-one.task.use-case';
+import { UpdateTaskUseCase } from '@/domain/use-cases/task/update.task.use-case';
+import { TaskRepositoryImpl } from '@/infra/repository/task.repository.impl';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskController } from './api/controllers/task.controller';
+import { CreateTaskUseCaseImpl } from './use-cases/create.task.use-case';
+import { DeleteTaskUseCaseImpl } from './use-cases/delete.task.use-case';
+import { ListByFilterTaskUseCaseImpl } from './use-cases/list-by-filter.task.use-case';
+import { ListOneTaskUseCaseImpl } from './use-cases/list-one.task.use-case';
+import { UpdateTaskUseCaseImpl } from './use-cases/update.task.use-case';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity, TaskEntity])],
+  controllers: [TaskController],
+  providers: [
+    UpdateTaskUseCaseImpl,
+    {
+      provide: UpdateTaskUseCase,
+      useExisting: UpdateTaskUseCaseImpl,
+    },
+    ListOneTaskUseCaseImpl,
+    {
+      provide: ListOneTaskUseCase,
+      useExisting: ListOneTaskUseCaseImpl,
+    },
+    ListByFilterTaskUseCaseImpl,
+    {
+      provide: ListByFilterTaskUseCase,
+      useExisting: ListByFilterTaskUseCaseImpl,
+    },
+    DeleteTaskUseCaseImpl,
+    {
+      provide: DeleteTaskUseCase,
+      useExisting: DeleteTaskUseCaseImpl,
+    },
+    CreateTaskUseCaseImpl,
+    {
+      provide: CreateTaskUseCase,
+      useExisting: CreateTaskUseCaseImpl,
+    },
+    TaskRepositoryImpl,
+    {
+      provide: TaskRepository,
+      useExisting: TaskRepositoryImpl,
+    },
+  ],
+})
+export class TaskModule {}
