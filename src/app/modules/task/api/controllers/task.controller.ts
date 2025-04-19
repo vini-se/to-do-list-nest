72716e2e904a84e1061023fed2b3ebbca1d6aff4
Vinicius <vinicius.dla2106@gmail.com>
2025-04-19 12:09:34 -0300

    feat: implements jwt and some bussiness rule

--- src/app/modules/task/api/controllers/task.controller.ts
+++ src/app/modules/task/api/controllers/task.controller.ts
@@ -1,3 +1,5 @@
+import { AuthGuard, IJwtRequest } from '@/app/modules/auth/guard/auth.guard';
+import { UserRoleEnum } from '@/commons/enum/user-role.enum';
 import { CreateTaskUseCase } from '@/domain/use-cases/task/create.task.use-case';
 import { DeleteTaskUseCase } from '@/domain/use-cases/task/delete.task.use-case';
 import { ListByFilterTaskUseCase } from '@/domain/use-cases/task/list-by-filter.task.use-case';
@@ -12,11 +14,16 @@ import {
   Patch,
   Post,
   Query,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
 import { CreateTaskDto } from '../dtos/create.task.dto';
 import { FilterTaskDto } from '../dtos/filter.task.dto';
 import { UpdateTaskDto } from '../dtos/update.task.dto';
 
+@UseGuards(AuthGuard)
+@ApiBearerAuth()
 @Controller('task')
 export class TaskController {
   constructor(
@@ -33,8 +40,13 @@ export class TaskController {
   }
 
   @Get()
-  async findByFilter(@Query() data: FilterTaskDto) {
-    return this.listByFilterUseCase.execute(data);
+  async findByFilter(@Query() data: FilterTaskDto, @Req() req: IJwtRequest) {
+    const userIdFromOperation = this.getUserIdFromRequest(req);
+
+    return this.listByFilterUseCase.execute({
+      ...data,
+      userId: userIdFromOperation,
+    });
   }
 
   @Delete(':id')
@@ -43,12 +55,38 @@ export class TaskController {
   }
 
   @Post()
-  async create(@Body() data: CreateTaskDto) {
-    return this.createTaskUseCase.execute(data);
+  async create(@Body() data: CreateTaskDto, @Req() req: IJwtRequest) {
+    const userIdFromOperation = this.getUserIdFromRequest(req);
+
+    return this.createTaskUseCase.execute({
+      ...data,
+      userId: userIdFromOperation,
+    });
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() data: UpdateTaskDto) {
-    return this.updateTaskUseCase.execute(id, data);
+  async update(
+    @Param('id') id: string,
+    @Body() data: UpdateTaskDto,
+    @Req() req: IJwtRequest,
+  ) {
+    const userIdFromOperation = this.getUserIdFromRequest(req);
+
+    return this.updateTaskUseCase.execute(id, {
+      ...data,
+      userId: userIdFromOperation,
+    });
+  }
+
+  private getUserIdFromRequest(req: IJwtRequest): string {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const userIdFromBody: string = req.body?.userId;
+
+    const userIdInOperation =
+      req.user.role === UserRoleEnum.ADMIN && userIdFromBody
+        ? userIdFromBody
+        : req.user.sub;
+
+    return userIdInOperation;
   }
 }
